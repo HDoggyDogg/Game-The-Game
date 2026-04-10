7e3519b08645ff070ed4c79cfe31e19df21a6374
HDoggyDogg <hderby@lisd.us>
2026-04-10 10:38:48 -0400

    Delete new-game-project-game directory

--- new-game-project-game/camera_2d.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-@onready var character_body_2d: CharacterBody2D = $"../CharacterBody2D"
-
-
-func _process(_delta):
-	if Globals.alive == true:
-		global_position = character_body_2d.global_position	
