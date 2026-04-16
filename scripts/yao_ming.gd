5fe8e97ecbd9ffba91244e70c9e9d75526284add
HDoggyDogg <hderby@lisd.us>
2026-04-16 09:26:53 -0400

    Add files via upload

---
+++ scripts/yao_ming.gd
@@ -0,0 +1,8 @@
+extends Area2D
+@onready var image: Sprite2D = $".."
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not Secrets.Yao_Ming:
+		Secrets.Yao_Ming = true
+		Globals.secrets_found += 1
